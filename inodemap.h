d62ccd6b81c2086e7b5a82a5823e60d3da8f3f77
Tilliano <rareaverage301@gmail.com>
2018-05-20 20:38:41 +0200

    Didn't read specification

--- inodemap.h
+++ inodemap.h
@@ -10,7 +10,7 @@ typedef struct
 	unsigned char* mappa;
 } InodeMap;
 
-// Dimensione totale della struttura = 1+1+1+1+1+4+4+4+4+1+(4*12)+4+4+4 = 82 -> sarebbe bello che fosse un divisore di 512, almeno non si spreca spazio nei blocchi
+// Dimensione totale della struttura = 1+1+1+1+1+4+4+4+4+1+4 = 31 -> sarebbe bello che fosse un divisore di 512, almeno non si spreca spazio nei blocchi
 typedef struct
 {
 	// unsigned char == uint8_t, per memorizzare byte / piccoli valori ( sizeof(unsigned char) = 1 )
@@ -33,9 +33,12 @@ typedef struct
 	unsigned long int dimensioneFile;
 	// Dimensione in blocchi (quanti blocchi occupa il file descritto da questo inode)
 	unsigned int dimensioneBlocchi;
-	// Tipo di file ( r = file regolare, d = directory, ecc... )
+	// Tipo di file ( 'r' = file regolare, 'd' = directory, ecc... )
 	char tipoFile;
+	// Indice del primo blocco del file (LINKED LIST BLOCKS)
+	unsigned int primoBlocco;
 	
+	/// NO NEED: non avevo lette le specifiche
 	// Gestione dell'indirizzamento ai blocchi contenente il contenuto del file
 	// Per ora massima dimensione massima file possibile con questi puntatori = 1GB ( circa 1 082 202 112 bytes )
 	// en.wikipedia.org/wiki/Inode_pointer_structure
@@ -44,24 +47,24 @@ typedef struct
 	// Puntatore ai blocchi diretti 12: contengono l'indice del blocco contenente il contenuto del file
 	// Possibiltà di accedere ai primi 12*(BLOCK_SIZE - HEADER_SIZE) del file 
 	// [] -> [contenuto del file]
-	int[12] puntatoriDiretti;
+	//int[12] puntatoriDiretti;
 	
 	// Puntatore indiritto ai blocchi: contiene l'indice del blocco contenente una lista di indici dei blocchi contenente 
 	// il contenuto del file
 	// [] -> [ | ], dove ogni quadratino punta al contenuto del file
-	int puntatoreIndiretto;
+	//int puntatoreIndiretto;
 	
 	// Puntatore doppiamente indiretto: contiene l'indice del blocco contenente una lista di indici dei blocchi, dove ciascun
 	// blocco contiene a sua volta una lista di indici dei blocchi contenente il contenuto del file
 	// [] -> [ | ], dove ogni quadratino -> [ | | ], infine questi puntano al blocco dove c'è il contenuto del file
-	int puntatoreDoppioIndiretto;
+	//int puntatoreDoppioIndiretto;
 	
 	// Puntatore triplamente indiretto: contiene l'indice del blocco contenente una lista di indici dei blocchi, dove ciascun
 	// blocco contiene a sua volta una lista di indici dei blocchi con altri indici dei blocchi dove questi ultimi hanno finalmente
 	// gli indici dei blocchi contenente il contenuto del file
 	// [] -> [ | ], dove ogni quadratino -> [ | | | ], ciascun quadratino punta ad un'altra lista di quadratini [ | | | | ] nella quale
 	// ogni quadratino punta ad un blocco contenente il contenuto del file
-	int puntatoreTriploIndiretto;
+	//int puntatoreTriploIndiretto;
 } Inode;
 
 typedef struct 
